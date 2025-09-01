ccf8fcbe8dd1d5a5921aa12b1da02d83f1cb14fd
root <root@DESKTOP-PAR04HV.localdomain>
2025-09-01 19:18:47 -0400

    third push

--- wrangling.py
+++ wrangling.py
@@ -3,34 +3,35 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+#loading the necessary packages 
 
 df=pd.read_csv("data/airbnb_hw.csv") #importing the data airbnb
 
 
 
 #print("df.shape:")
-#print(df.shape, '\n') # List the dimensions of df
+print(df.shape, '\n') # List the dimensions of df
 
 #print("df.dtypes:")
-#print(df.dtypes, '\n') # The types of the variables; `object` is a bad sign
+print(df.dtypes, '\n') # The types of the variables; `object` is a bad sign
 
 #print("df.columns:")
-#print(df.columns, '\n')
+print(df.columns, '\n')
 
 pd.set_option('display.max_rows', None)
 
-# Optional: show all columns too
+# I did this code so I could have my dataset show me all of the rows and columns
 pd.set_option('display.max_columns', None)
 
+# Question 1, Part 1
 
-
-x=df.isnull()
-#print(x)
+x=df['Price'].isna().sum()
+print(x)
 #https://www.geeksforgeeks.org/data-analysis/working-with-missing-data-in-pandas/ (citations for my work, it printed out no null values)
 
 # there are some commas when the price goes over 1,000, so we need to remove these commas so the variable is numeric
 
-#df['Price'] = pd.to_numeric(df['Price'], errors='coerce')
+
 df['Price']=df['Price'].str.replace(",", "") #removing the commas, did a print statement later to check and the comma was gone
 
 
@@ -40,16 +41,16 @@ df2=pd.read_csv("data/mn_police_use_of_force.csv") #reading in the second datase
 
 
 df2['subject_injury'] = df2['subject_injury'].replace(' ',np.nan) #replacing the missing values with np.nan
-#print(df2.head(50)) #shows the NaNs are in there
+print(df2.head(50)) #shows the NaNs are in there
 na=(df2['subject_injury'].isna().sum())
 length=len(df2['subject_injury'])
-#print(na/length)
+print(na/length)
 # equals .76; this is concerning because there are a lot of missing values, and this column only has 1/4 actual data. it is not very reliable.
 #https://stackoverflow.com/questions/26266362/how-do-i-count-the-nan-values-in-a-column-in-pandas-dataframe
-#print(df2['subject_injury'].sum())
+print(df2['subject_injury'].sum())
 
-#print(pd.crosstab(df2['subject_injury'],df2['force_type']))
-# baton, firearms, less lethal projectiles are the very concerning ones because they don't have many issues
+print(pd.crosstab(df2['subject_injury'],df2['force_type'])) #this is to cross tabulate 
+# baton, firearms, less lethal projectiles are the very concerning ones because they don't have many data entries in the cross tabulation 
 
 
 # Question 3
@@ -58,11 +59,11 @@ df3 = pd.read_csv(url,low_memory=False)
 
 
 
-#NA=(df3['WhetherDefendantWasReleasedPretrial'].isna().sum())
-#print(NA)
-#print(df3['WhetherDefendantWasReleasedPretrial'].unique())
+NA=(df3['WhetherDefendantWasReleasedPretrial'].isna().sum())
+print(NA)
+print(df3['WhetherDefendantWasReleasedPretrial'].unique())
 # both of these codes showed that 1) there were no NA values, and 2) there is a 9 in the data column so we need to remove that
-#df3['WhetherDefendantWasReleasedPretrial']=df3['WhetherDefendantWasReleasedPretrial'].replace('9',np.nan) #this will replace the 9 with an NAN
+df3['WhetherDefendantWasReleasedPretrial']=df3['WhetherDefendantWasReleasedPretrial'].replace('9',np.nan) #this will replace the 9 with an NAN
 
 
 # Part 4
--- wrangling2.py
+++ wrangling2.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3
 
+# Question 2
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# importing the necessary packages 
 
-
-
-df4 = pd.read_excel("/mnt/c/Users/rache/Downloads/GSAF5.xls")
+# Part 1
+df4 = pd.read_excel("/mnt/c/Users/rache/Downloads/GSAF5.xls") #reading in the file
 #https://www.geeksforgeeks.org/python/reading-excel-file-using-python/
 
 pd.set_option('display.max_rows', None)
 
-# Optional: show all columns too
-pd.set_option('display.max_columns', None)
 
+pd.set_option('display.max_columns', None)
+#https://builtin.com/data-science/pandas-show-all-columns
+
+#Part 2
 #print(df4.columns)
 length=len(df4['Time'])
 print(length)
 
 c=df4.isna().sum()
 print(c)
+# i dropped the unnamed columns because they weren't in the excel dataset and I wasn't sure what they meant, otherwise all of the columns had data in them
 
 df4 = df4.drop(['Unnamed: 21', 'Unnamed: 22'], axis=1)
 print(df4.columns)
+#https://www.geeksforgeeks.org/python/how-to-drop-one-or-multiple-columns-in-pandas-dataframe/
 
-
+#Part 3
 print(df4['Year'].unique())
 
 print(df4['Year'].head(20))
 
 # I see a range of values going from 0000-2025. To clean the column, I did the following code to remove missing values.
 df4['Year'] = df4['Year'].replace(' ',np.nan)
-filtered_df4 = df4[df4['Year'] > 1940]
+filtered_df4 = df4[df4['Year'] > 1939]
 print(filtered_df4['Year'].unique())
+
 
 #https://www.geeksforgeeks.org/pandas/ways-to-filter-pandas-dataframe-by-column-values/
 filtered_df4['Year'].hist(bins=50)
@@ -41,32 +47,38 @@ plt.show()
 plt.savefig("year_hist.png")
 # i did a histogram and judging from the histogram, the shark attacks are increasing as the years progress
 
-filtered_df4['Age'] = filtered_df4['Age'].replace([' ','nan','NA','N/A'], np.nan)
-filtered_df4['Age'] = pd.to_numeric(filtered_df4['Age'], errors='coerce')
-filtered_df4['Age'].hist(bins=50)
+# Part 4
+df4['Age']=df4['Age'].astype(str)
+df4['Age']=df4['Age'].str.strip()
+df4['Age'] = df4['Age'].replace([' ','nan','NA','N/A'], np.nan)
+
+df4['Age'].hist(bins=50)
 plt.show()
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
 
-plt.savefig("AGE_histo.png")
 
+plt.savefig("AGE.png")
+# saving the histogram to a picture
 
-print(filtered_df4['Age'].describe())
-print(filtered_df4['Age'].unique())
+#Part 5
 
-print(len(filtered_df4[filtered_df4['Sex']=='M']))
-print(len(filtered_df4[filtered_df4['Sex']=='F']))
 
 x=len(df4[df4['Sex']=='M'])
 y=len(df4[df4['Sex']=='F'])
 print(x/(x+y))
-#male is 4311/5026, and the 5026 comes from just entries of "M" or "F" (for the 1940 filtered data)
+
 #male proportion is .88 for the unfiltered data
-print(df4['Type'].unique())
+
+# Part 6
+
+print(df4['Type'].unique()) #seeing all the unique types of entries in the column
 
 df4['Type'] = df4['Type'].replace(['Watercraft', 'Sea Disaster','Boat','unprovoked'], 'Unprovoked')
 df4['Type'] = df4['Type'].replace(' Provoked', 'Provoked')
 df4['Type'] = df4['Type'].replace(['Questionable','?','Unconfirmed','Unverified','Invalid','Under investigation'], 'Unknown')
 df4['Type'] = df4['Type'].replace([' ', 'nan'], np.nan)
 print(df4['Type'].unique())
+#this is me removing the extra words and narrowing it down to three different categories, and I did a print statement to check my work
 
 a=len(df4['Type'])
 b=len(df4[df4['Type']=='Unprovoked'])
@@ -75,7 +87,9 @@ print(ratio)
 
 # 0.82 of the attacks were unprovoked
 
-print(df4['Fatal Y/N'].unique())
+#Part 7
+
+print(df4['Fatal Y/N'].unique()) #seeing the unique values in the column
 
 
 df4['Fatal Y/N']=df4['Fatal Y/N'].replace(['F','M','Nq','UNKNOWN',2017,'Y x 2'],np.nan)
@@ -83,11 +97,14 @@ df4['Fatal Y/N']=df4['Fatal Y/N'].replace(['n',' N', 'N '], 'N')
 df4['Fatal Y/N']=df4['Fatal Y/N'].replace('y','Y')
 
 print(df4['Fatal Y/N'].unique())
+# cleaning the column with .replace and then checking my work to make sure there are only Y or N
+
+#Part 8
 
 q1=df4[['Type','Sex']]
 q2=df4[['Type','Fatal Y/N']]
 q3=df4[['Fatal Y/N', 'Sex']]
-
+# making three subsets
 print(len(q1[(q1['Type']=='Unprovoked') & (q1['Sex']=='M')]))
 print(len(q1[(q1['Type']=='Unprovoked') & (q1['Sex']=='F')]))
 # there are much more males with unprovoked shark attacks than females
@@ -102,6 +119,8 @@ print(len(q3[(q3['Sex']=='F') & (q3['Fatal Y/N']=='Y')]))
 
 # I still don't like sharks but I feel better because I am a female
 
+# Part 9
+
 gw=df4['Species '].str.count('Great White')
 print(gw.count())
 gw1=df4['Species '].str.count('white')
@@ -109,6 +128,6 @@ gw2=df4['Species '].str.count('great')
 print(gw1.count())
 print(gw2.count())
 # all of these print statements produced 3911
-
+# https://www.w3schools.com/python/ref_string_count.asp
 print(len(df4['Species ']))
 # 3911/7042 is the proportion of attacks from white sharks 
